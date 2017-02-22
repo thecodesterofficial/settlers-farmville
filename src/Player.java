12dcea6bfd43a6de368b8a0936f0ff9a8a89ae6c
cody short <cody95@vt.edu>
2017-02-21 17:46:01 -0800

    Added initial files

---
+++ src/Player.java
@@ -0,0 +1,103 @@
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+
+public class Player {
+	static GameBoard game;
+    ObjectInputStream in;
+    ObjectOutputStream out;
+    static JFrame frame = new JFrame("Dummy Frame");
+    
+
+
+
+
+    @SuppressWarnings("resource")
+	private void run(String serverAddress, String portNumber) throws IOException {
+
+     try{
+
+        System.out.println("inside run");
+        Socket socket = new Socket(serverAddress, Integer.parseInt(portNumber));
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+       
+       // out.writeObject((Object)"hello this is the player speaking");
+        
+     }
+     catch(java.net.SocketException e){
+    	JOptionPane.showMessageDialog(frame, "Couldn't connect to that IP adress and Port number");
+    	System.exit(0);
+     }
+
+
+
+              System.out.println("before while loop");
+             
+             while(true){
+            	 
+            	 System.out.println("after while loop");
+				try {
+					System.out.println("inside try");
+					System.out.println(in.readObject().toString());
+					game = (GameBoard) in.readObject();
+					System.out.println("after read object");
+					if(game!=null){
+						System.out.println("yay!");
+						UserInterface.init(game);
+					}else{
+						System.out.println("object was null");
+					}
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            	  
+         
+            	  
+            }
+              
+              
+    }
+    
+    
+
+
+ 
+    public static void start() throws Exception {
+    	
+        String serverAddress = JOptionPane.showInputDialog(
+        		frame,
+                "Enter IP Address of the Server:",
+                "Settlers of Farmville",
+                JOptionPane.QUESTION_MESSAGE);
+        
+        String portNumber = JOptionPane.showInputDialog(
+        		frame,
+                "Enter Port Number:",
+                "Settlers of Farmville!",
+                JOptionPane.QUESTION_MESSAGE);
+        
+        String username = JOptionPane.showInputDialog(
+        		frame,
+                "Choose a unique username",
+                "Settlers of Farmville!",
+                JOptionPane.PLAIN_MESSAGE);
+        
+        System.out.println(username);
+        System.out.println(serverAddress);
+        System.out.println(Integer.parseInt(portNumber));
+    	
+    	
+       Player player = new Player();
+        player.run(serverAddress, portNumber);
+    }
+}

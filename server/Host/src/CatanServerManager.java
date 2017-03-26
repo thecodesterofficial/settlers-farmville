d78a8ffedb05fdbc73c2b5e911f69155515161b0
Cody Short <stepintostem@gmail.com>
2017-03-26 19:16:14 -0400

    Changed connections to array to make it cleaner

--- server/Host/src/CatanServerManager.java
+++ server/Host/src/CatanServerManager.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CatanServerManager {
 	
 	final private ServerSocket socket;
     final public static int MAX_CLIENTS = 4;
-    final private CatanServer[] connections = new CatanServer[ MAX_CLIENTS ];
+    //final private CatanServer[] connections = new CatanServer[ MAX_CLIENTS ];
+    List<CatanServer> connections = new ArrayList<CatanServer>();
     private int turn = 0;
     public CatanServerManager( int port ) throws IOException {
     	this.socket = new ServerSocket( port );
@@ -28,9 +30,13 @@ public class CatanServerManager {
     }
     public void Dispatch(String message)
     {
-    	for ( int i = 0 ; i < MAX_CLIENTS && this.connections[ i ] != null; i++ ) {    
+    	/*for ( int i = 0 ; i < MAX_CLIENTS && this.connections[ i ] != null; i++ ) {    
             connections[i].Send(message);
-        }
+        }*/
+    	for(CatanServer conn : connections)
+    	{
+    		conn.Send(message);
+    	}
     }
     public void  NewPlayer(String name)
     {
@@ -38,7 +44,7 @@ public class CatanServerManager {
     }
     public void StartGame()
     {
-    	Dispatch("game start");
+    	Dispatch("game init");
     	board = new GameBoard();
     	List<Hex> hexes = board.GetHexes();
     	for(int i = 0; i < hexes.size(); i++)
@@ -46,25 +52,33 @@ public class CatanServerManager {
     		Hex hex = hexes.get(i);
     		Dispatch("game init hex " + i + " " + hex.GetType());
     	}
-    	
+    	Dispatch("game start");
     	
     	Dispatch("player turn " + this.turn);
     	
     }
     
     public void assignConnection( Socket connection ) {
-         for ( int i = 0 ; i < MAX_CLIENTS ; i++ ) {            
+         /*for ( int i = 0 ; i < MAX_CLIENTS ; i++ ) {            
              if ( this.connections[ i ] == null ) {
             	  try
             	  {
             		  this.connections[ i ] = new CatanServer( connection , i, this);
-            	  } catch(IOException e)
-            	  {
-            		  System.out.println(e.getMessage());
-            	  }
+            	  } 
                   break;
              }
-         }
+         }*/
+    	if(this.connections.size() < MAX_CLIENTS)
+    	{
+    		try
+    		{
+    			this.connections.add(new CatanServer(connection, connections.size(), this));
+    		}
+    		catch(IOException e)
+      	  	{
+      		  	System.out.println(e.getMessage());
+      	  	}
+    	}
     }
 	public static void main(String[] args) {
 		int port = 9534;
